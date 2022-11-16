a6c3e0349e225d2dcac572d19a294326befabd15
mimaline <yasmim.aline97@gmail.com>
2022-11-16 14:08:50 -0300

    ok

--- atividade04/exercicio_07.php
+++ atividade04/exercicio_07.php
@@ -4,4 +4,4 @@ $estados = array ("SC"=> array ("1"=>"Rio do Sul", "2"=>"Joinville", "3"=>"Blume
                   "RS"=> array ("1"=>"Porto Alegre", "2"=>"Caxias do Sul", "3"=>"Santa Rosa"),
                   "PR"=> array ("1"=>"Curitiba", "2"=>"Pato Branco", "3"=>"Londrina"));
 
-print_r($estados);
+echo "<pre>" .print_r($estados, true) ."</pre>";
---
+++ atividade04/exercicio_08.php
@@ -0,0 +1,8 @@
+<?php
+
+$time = array ("Joinville"=> array ("posicao"=>"2", "pontos"=>"5", "saldo_gols"=>"4"),
+               "Chapecoense"=> array ("posicao"=>"1", "pontos"=>"9", "saldo_gols"=>"6"),
+               "Figuerense"=> array ("posicao"=>"3", "pontos"=>"3", "saldo_gols"=>"0"),
+               "Avaí"=> array ("posicao"=>"4", "pontos"=>"1", "saldo_gols"=>"-2"), );
+
+               echo "<pre>" .print_r($time, true) ."</pre>";
